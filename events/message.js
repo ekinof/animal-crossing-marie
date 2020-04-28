99c3d519457c9285f4b6fcb4ef58fccaaf23163d
Artwo <hello@artwo.xyz>
2020-04-28 16:49:13 +0200

    Update messages.js - Add replies if the bot is mentioned
    New replies for the bot if it's mentioned

--- events/message.js
+++ events/message.js
@@ -4,9 +4,17 @@ const vdb = require("../commands/vdb");
 module.exports = (client, message) => {
   // console.log(message.content)
   if (message.content.startsWith("!passeport")) {
-    return passport(message)
+    if (message.mentions.has(client.user)) { // If the bot is mentionned
+      return message.reply("je n'ai pas à montrer mes papiers, je travaille ici.")
+    } else {
+      return passport(message)
+    }
   }
   if (message.content.startsWith("!vdb")) {
-    return vdb(message)
+    if (message.mentions.has(client.user)) { // If the bot is mentionned
+      return message.reply("c'est gentil de penser à moi mais j'ai tout ce qu'il me faut déjà :relaxed:")
+    } else {
+      return vdb(message)
+    }
   }
 }
